7642df82c0f2a42a298ce1080104b36bdf0d2713
ksenia1997 <bolshakovaks@icloud.com>
2020-02-12 10:51:51 +0100

    update seq2seq_multiK to check if an error in vocab

--- seq2seq_multiK.py
+++ seq2seq_multiK.py
@@ -41,7 +41,7 @@ def training(model, iterator, optimizer, criterion):
     Returns:
         epoch_loss: Average loss of the epoch.
     '''
-    print("Train")
+    print("Train multi K")
     #  some layers have different behavior during train/and evaluation (like BatchNorm, Dropout) so setting it matters.
     model.train()
     # loss
@@ -87,7 +87,7 @@ def evaluating(model, iterator, criterion):
 
     model.eval()
     epoch_loss = 0
-    print("Evaluate")
+    print("Evaluate multi K")
     # we don't need to update the model parameters. only forward pass.
     with torch.no_grad():
         for i, batch in enumerate(iterator):
@@ -101,6 +101,104 @@ def evaluating(model, iterator, criterion):
             # if (i + 1) % 100 == 0:
             #    print("eval loss: ", epoch_loss / i)
     return epoch_loss / len(iterator)
+
+
+class EncoderM(nn.Module):
+    def __init__(self, config, vocab):
+        super().__init__()
+
+        self.embedding_dim = config["embedding_dim"]
+        self.hidden_dim = config["hidden_dim"]
+        self.n_layers = config["num_layers"]
+        self.dropout_rate = config['dropout_rate']
+        self.vocab_size = len(vocab)
+        self.embedder = nn.Embedding(self.vocab_size, self.embedding_dim).to(device)
+        self.lstm = torch.nn.LSTM(
+            self.embedding_dim,
+            self.hidden_dim,
+            self.n_layers,
+            dropout=float(self.dropout_rate)).to(device)
+        self.dropout = nn.Dropout(self.dropout_rate).to(device)
+
+    def forward(self, input_sequence):
+        # Convert input_sequence to word embeddings
+        embeds_q = self.embedder(input_sequence[0]).to(device)
+        embedded = self.dropout(embeds_q).to(device)
+        inp_packed = pack_padded_sequence(embedded, input_sequence[1], batch_first=False, enforce_sorted=False)
+        outputs, (hidden, cell) = self.lstm(inp_packed)
+        outputs, output_lengths = pad_packed_sequence(outputs, batch_first=False,
+                                                      padding_value=input_sequence[0][0][0],
+                                                      total_length=input_sequence[0].shape[0])
+        return outputs, hidden, cell
+
+
+class DecoderM(nn.Module):
+    def __init__(self, config, vocab):
+        super().__init__()
+        self.embedding_dim = config["embedding_dim"]
+        self.hidden_dim = config["hidden_dim"]
+        self.output_dim = len(vocab)
+        self.n_layers = config["num_layers"]
+        self.dropout_rate = config['dropout_rate']
+        self.embedder = nn.Embedding(self.output_dim, self.embedding_dim).to(device)
+        self.lstm = torch.nn.LSTM(
+            self.embedding_dim,
+            self.hidden_dim,
+            self.n_layers,
+            dropout=float(self.dropout_rate)).to(device)
+        self.linear = nn.Linear(self.hidden_dim, self.output_dim).to(device)
+        self.dropout = nn.Dropout(self.dropout_rate).to(device)
+
+    def forward(self, inputs, hidden, cell):
+        inputs = inputs.unsqueeze(0)
+        # inputs [1,batch_size]
+        embedded = self.embedder(inputs).to(device)
+        embedded = self.dropout(embedded).to(device)
+        # embedded [1, batch_size, embedded_dim]
+        output, (hidden, cell) = self.lstm(embedded, (hidden, cell))
+        predicted = self.linear(output.squeeze(0)).to(device)
+        return predicted, hidden, cell
+
+
+class Seq2SeqM(nn.Module):
+    def __init__(self, config, vocab_src, vocab_trg):
+        super().__init__()
+        self.encoder = EncoderM(config, vocab_src).to(device)
+        if WITH_ATTENTION:
+            self.decoder = LuongDecoder(config, vocab_trg).to(device)
+        else:
+            self.decoder = DecoderM(config, vocab_trg).to(device)
+
+        assert self.encoder.hidden_dim == self.decoder.hidden_dim, \
+            "Hidden dimensions of encoder and decoder must be equal!"
+        assert self.encoder.n_layers == self.decoder.n_layers, \
+            "Encoder and decoder must have equal number of layers!"
+
+        experiment_name = "train_" + time.strftime('%d-%m-%Y_%H:%M:%S')
+        tensorboard_log_dir = './tensorboard-logs/{}/'.format(experiment_name)
+        self.tb = SummaryWriter(tensorboard_log_dir)
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        # src [seq_len, batch_size]
+        # trg [seq_len, batch_size]
+        max_len, batch_size = trg[0].size()
+        trg_vocab_size = self.decoder.output_dim
+        outputs = torch.zeros(max_len, batch_size, trg_vocab_size).to(device)
+        enc_output, hidden, cell = self.encoder(src)
+        enc_output = enc_output.permute(1, 0, 2)
+        decoder_h = (hidden, cell)
+        decoder_input = trg[0][0, :]
+        for t in range(1, max_len):
+            if WITH_ATTENTION:
+                output, decoder_h, attn_weights = self.decoder(decoder_input, decoder_h, enc_output)
+            else:
+                output, hidden, cell = self.decoder(decoder_input, hidden, cell)
+            outputs[t] = output
+            use_teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.max(1)[1]
+            decoder_input = trg[0][t] if use_teacher_force else top1
+
+        return outputs.to(device)
 
 
 def model_fit(model, train_iter, valid_iter):
@@ -149,7 +247,8 @@ train_data, valid_data, test_data = Multi30k.splits(exts=('.de', '.en'),
 SRC.build_vocab(train_data, vectors=GloVe(name='6B', dim=config["embedding_dim"]))
 TRG.build_vocab(train_data, vectors=GloVe(name='6B', dim=config["embedding_dim"]))
 
-vocab = SRC.vocab
+vocab_src = SRC.vocab
+vocab_trg = TRG.vocab
 print(f"Unique tokens in source (de) vocabulary: {len(SRC.vocab)}")
 
 BATCH_SIZE = 128
@@ -160,5 +259,5 @@ train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
     device=device)
 
 SRC_PAD_IDX = SRC.vocab.stoi[SRC.pad_token]
-model = Seq2Seq(config, vocab)
+model = Seq2SeqM(config, vocab_src, vocab_trg)
 model_fit(model, train_iterator, valid_iterator)
