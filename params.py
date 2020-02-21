511892b296f7dbabe1d7047e6c09507f9c83b2ae
ksenia1997 <bolshakovaks@icloud.com>
2020-02-21 15:55:23 +0100

    traned model with preprocessing

--- params.py
+++ params.py
@@ -10,7 +10,7 @@ DATA_PATH = "./datasets/"
 MODEL_PATH = "./models/"
 
 # Preprocess
-DATA_TYPE = "PERSONA_BOTH"  # TWITTER or PERSONA or PERSONA_BOTH (PERSONA_BOTH contains descriptions of both people)
+DATA_TYPE = "TWITTER"  # TWITTER or PERSONA or PERSONA_BOTH (PERSONA_BOTH contains descriptions of both people)
 WITH_DESCRIPTION = True
 CONTEXT_PAIR_COUNT = 0
 
@@ -24,7 +24,7 @@ IS_TEST = True
 
 MODEL_SAVE_PATH = MODEL_PATH + 'seq2seq_model.pt'
 
-config = {"train_batch_size": 10, "optimize_embeddings": False,
+config = {"train_batch_size": 32, "optimize_embeddings": False,
           "embedding_dim": 300, "hidden_dim": 512, "dropout_rate": 0.1, "num_layers": 2,
           "attention_model": 'concat', "transformer_d_model": 512, "transformer_heads": 8, "transformer_n": 6,
           "transformer_dropout": 0.1}
--- utils/json.py
+++ utils/json.py
@@ -14,17 +14,22 @@ def load_json(filename):
 def process_data_to_json(filename, data):
     # data contains line by line source, target.
     json_data = []
-    for i in range(0, len(data), 2):
+    length_data = len(data)
+    if len(data) % 2 != 0:
+        length_data = len(data)-1
+    for i in range(0, length_data, 2):
         json_data.append({'src': data[i], 'trg': data[i + 1]})
 
     create_json(filename, json_data)
 
 
 def process_data_for_BART(filename, data):
+    print(len(data))
     with open(filename + ".source", 'w') as source, open(filename + ".target", 'w') as target:
-        for i in range(0, len(data), 2):
-            source.write(data[i])
-            target.write(data[i])
+        for i in range(0, 100, 2):
+            source.write(data[i]+'\n')
+            target.write(data[i+1]+'\n')
 
     source.close()
     target.close()
+
