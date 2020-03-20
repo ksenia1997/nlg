c718efd700f7d06fda0a2f68ad1158bcae86eccc
ksenia1997 <bolshakovaks@icloud.com>
2020-03-20 14:35:45 +0100

    bart and gpt2 join test

--- mixed_bart/join_source_and_hypo.py
+++ mixed_bart/join_source_and_hypo.py
@@ -3,16 +3,18 @@ from utils.csv_process import save_to_csv
 
 def join_test_source_and_hypo():
     lines = []
-    with open('.data/test.source', "r") as source, open('mixed_bart/test_idf.hypo', "r") as hypo:
+    with open('.data/test.source', "r") as source, open('mixed_bart/test_combined.hypo', "r") as hypo:
         count = 0
         hypo_data = hypo.readlines()
         for src in source:
             lines.append(src)
             lines.append(hypo_data[count])
             count += 1
+            if count == 21:
+                break
     source.close()
     hypo.close()
-    save_to_csv("mixed_bart/bart_result__tfidf.csv", lines)
+    save_to_csv("mixed_bart/bart_gpt2_7_3.csv", lines)
 
 
 join_test_source_and_hypo()
